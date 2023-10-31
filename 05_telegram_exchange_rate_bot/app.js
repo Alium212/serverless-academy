3ae34a85e584aba7a3bbd950c3817c8ecc4669b9
AlexDen212 <47757689+AlexDen212@users.noreply.github.com>
2023-11-01 01:30:32 +0200

    05_telegram_exchange_rate_bot: bug fixed

--- 05_telegram_exchange_rate_bot/app.js
+++ 05_telegram_exchange_rate_bot/app.js
@@ -1,20 +1,20 @@
-const axios = require("axios");
-const TelegramBot = require("node-telegram-bot-api");
-const NodeCache = require("node-cache");
+const axios = require('axios');
+const TelegramBot = require('node-telegram-bot-api');
+const NodeCache = require('node-cache');
 
-const token = "6418499223:AAEMTxoVI-KSfMyYRe7gUn9_LyV-KItyowo";
+const token = '6418499223:AAEMTxoVI-KSfMyYRe7gUn9_LyV-KItyowo';
 const bot = new TelegramBot(token, { polling: true });
 const cache = new NodeCache({ stdTTL: 60 });
 
-const privatBankApi = "https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5";
-const monoBankApi = "https://api.monoBank.ua/bank/currency";
+const privatBankApi = 'https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5';
+const monoBankApi = 'https://api.monoBank.ua/bank/currency';
 
 const getPrivatBankRates = async () => {
     try {
         const response = await axios.get(privatBankApi);
         return response.data;
     } catch (error) {
-        console.error("Error fetching PrivatBank rates:", error);
+        console.error('Error fetching PrivatBank rates:', error);
         return null;
     }
 };
@@ -24,7 +24,7 @@ const getMonoBankRates = async () => {
         const response = await axios.get(monoBankApi);
         return response.data;
     } catch (error) {
-        console.error("Error fetching monoBank rates:", error);
+        console.error('Error fetching monoBank rates:', error);
         return null;
     }
 };
@@ -46,15 +46,15 @@ const updateCachedData = async () => {
     const usdRatesMessage = `USD Exchange Rates:\n\nPrivatBank: ${getExchangeString(rates.usd.privatBankBuy, rates.usd.privatBankSell)}\nMonoBank: ${getExchangeString(rates.usd.monoBankBuy, rates.usd.monoBankSell)}`;
     const eurRatesMessage = `EUR Exchange Rates:\n\nPrivatBank: ${getExchangeString(rates.eur.privatBankBuy, rates.eur.privatBankSell)}\nMonoBank: ${getExchangeString(rates.eur.monoBankBuy, rates.eur.monoBankSell)}`;
 
-    cache.set("usdRates", usdRatesMessage, 60);
-    cache.set("eurRates", eurRatesMessage, 60);
-    cache.set("privatBankRates", privatBankRates, 60);
-    cache.set("monoBankRates", monoBankRates, 60);
+    cache.set('usdRates', usdRatesMessage, 60);
+    cache.set('eurRates', eurRatesMessage, 60);
+    cache.set('privatBankRates', privatBankRates, 60);
+    cache.set('monoBankRates', monoBankRates, 60);
 };
 
 const formatRates = (privatBankRates, monoBankRates) => {
-    const privatBankUSD = privatBankRates.find((rate) => rate.ccy === "USD");
-    const privatBankEUR = privatBankRates.find((rate) => rate.ccy === "EUR");
+    const privatBankUSD = privatBankRates.find((rate) => rate.ccy === 'USD');
+    const privatBankEUR = privatBankRates.find((rate) => rate.ccy === 'EUR');
     const monoBankUSD = monoBankRates.find(
         (rate) => rate.currencyCodeA === 840 && rate.currencyCodeB === 980
     );
@@ -79,7 +79,7 @@ const formatRates = (privatBankRates, monoBankRates) => {
 };
 
 const keyboard = {
-    keyboard: [["USD", "EUR"]],
+    keyboard: [['USD', 'EUR']],
     one_time_keyboard: false,
     resize_keyboard: true,
 };
@@ -92,7 +92,7 @@ const sendCachedRates = (chatId, currency) => {
             reply_markup: keyboard,
         });
     } else {
-        bot.sendMessage(chatId, "Exchange rate data is not available at the moment.", {
+        bot.sendMessage(chatId, 'Exchange rate data is not available at the moment.', {
             reply_markup: keyboard,
         });
     }
@@ -103,7 +103,7 @@ bot.onText(/\/start/, (msg) => {
 
     bot.sendMessage(
         chatId,
-        "Welcome! You can check exchange rates with this bot.",
+        'Welcome! You can check exchange rates with this bot.',
         {
             reply_markup: keyboard,
         }
@@ -112,24 +112,24 @@ bot.onText(/\/start/, (msg) => {
 
 bot.onText(/USD/, async (msg) => {
     const chatId = msg.chat.id;
-    const privatBankRates = cache.get("privatBankRates");
-    const monoBankRates = cache.get("monoBankRates");
+    const privatBankRates = cache.get('privatBankRates');
+    const monoBankRates = cache.get('monoBankRates');
 
     if (!privatBankRates || !monoBankRates) {
         await updateCachedData();
     }
 
-    sendCachedRates(chatId, "usd");
+    sendCachedRates(chatId, 'usd');
 });
 
 bot.onText(/EUR/, async (msg) => {
     const chatId = msg.chat.id;
-    const privatBankRates = cache.get("privatBankRates");
-    const monoBankRates = cache.get("monoBankRates");
+    const privatBankRates = cache.get('privatBankRates');
+    const monoBankRates = cache.get('monoBankRates');
 
     if (!privatBankRates || !monoBankRates) {
         await updateCachedData();
     }
 
-    sendCachedRates(chatId, "eur");
+    sendCachedRates(chatId, 'eur');
 });
